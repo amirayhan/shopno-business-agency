9696bbfa2b41315839ba227335b3fa58d039a698
rayhan <jaber.ac1983@gmail.com>
2023-09-05 08:18:30 +0600

    features section is added

---
+++ src/components/FeatureItem/FeatureItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FeatureItem = ({ title, icon }) => {
+    return (
+        <div className="feature_item p-10 sm:p-5 bg-gray-100 rounded-[20px] sm:col-span-2 xs:col-span-2">
+            <span className="text-5xl sm:text-4xl text-[#ff6809]">{icon}</span>
+            <h3 className="py-3 sm:text-3xl">{title}</h3>
+            <p>We make the over insurance quoting purchasing.</p>
+        </div>
+    );
+};
+
+export default FeatureItem;
