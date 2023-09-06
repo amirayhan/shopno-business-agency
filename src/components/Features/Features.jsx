9a5be37d7c3fb0ffe72c51ade8d04188c60b0482
amirayhan <jaber.ac1983@gmail.com>
2023-09-06 09:58:21 +0600

    new Update

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,19 +1,21 @@
+import { faChartLine, faHandshake, faTimeline } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import FeatureItem from "../FeatureItem/FeatureItem";
-import { faHandshake, faChartLine, faTimeline } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Features = () => {
     return (
         <div className="w-full bg-white py-28">
-            <div className="md:max-w-[1480px] max-w-[600px] m-auto grid grid-cols-5 gap-5 items-center">
-                <div className="features_heading md:col-span-2 sm:col-span-5 xs:col-span-5">
+            <div className="md:max-w-[1480px] max-w-[600px] m-auto grid grid-cols-3 gap-5 items-center">
+                <div className="features_heading">
                     <h4 className="text-4xl text-[#ff6809]">Features</h4>
                     <h2 className=" sm:text-5xl leading-[60px]">We Are The Best Agency To Improve Your Deals.</h2>
                 </div>
-                <FeatureItem icon={<FontAwesomeIcon icon={faHandshake} />} title={"business Advice"}></FeatureItem>
-                <FeatureItem icon={<FontAwesomeIcon icon={faChartLine} />} title={"Financial Advice"}></FeatureItem>
-                <FeatureItem icon={<FontAwesomeIcon icon={faTimeline} />} title={"Risk Management"}></FeatureItem>
+                <div className="feature_items col-span-2">
+                    <FeatureItem icon={<FontAwesomeIcon icon={faHandshake} />} title={"business Advice"}></FeatureItem>
+                    <FeatureItem icon={<FontAwesomeIcon icon={faChartLine} />} title={"Financial Advice"}></FeatureItem>
+                    <FeatureItem icon={<FontAwesomeIcon icon={faTimeline} />} title={"Risk Management"}></FeatureItem>
+                </div>
             </div>
         </div>
     );
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className="md:max-w-[1480px] max-w-[600px] m-auto grid grid-cols-2 items-center">
                 <div className="hero_content ">
                     <span className="px-6 py-3 bg-blue-100 sm:text-1xl text-blue-600 font-bold rounded-full inline-block">WE ARE EXPERT IN THIS FIELD</span>
-                    <h1 className="mt-5 sm:text-5xl md:text-8xl lg:text-9xl">We Help Grow Up Your Business</h1>
+                    <h1 className="mt-5 sm:text-5xl md:text-7xl lg:text-8xl">We Help Grow Up Your Business</h1>
                     <p>Acquiring new clients is one of the biggest challenges many consultants face. You need to convince them that you are the right person to fix their problems.</p>
                     <button className="px-6 py-3 theme_bg_color text-white font-bold rounded-md hover:bg-[#222] transition-all hover:ease-linear duration-300 mt-4">Our Services</button>
                 </div>
